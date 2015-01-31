e835f24c2f83008e5f0aebf2fca6e93e4612719b
siddkorg <sidd.korg@gmail.com>
2015-01-31 11:31:40 +0530

    task: sk-file updated

--- dm-common/src/main/java/org/ngo/think/dm/common/dto/DonorDTO.java
+++ dm-common/src/main/java/org/ngo/think/dm/common/dto/DonorDTO.java
@@ -1,224 +1,178 @@
 package org.ngo.think.dm.common.dto;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
 
-import org.codehaus.jackson.annotate.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class DonorDTO implements Serializable
+
 {
-
-	private static final long serialVersionUID = 1L;
-
+	private boolean selectedDonor;
 	private Long donorId;
 	private String donorUuid;
 	private String firstName;
-	private String lastName;
 	private String middleName;
+	private String lastName;
 	private Date birthDate;
+	
 	private String gender;
 	private String bloodGroup;
+	
 	private String donationComponentType;
-	private Integer rating;
+	private int rating;
+	
 	private String createdBy;
 	private Date createdDate;
 	private String modifiedBy;
 	private Date modifiedDate;
+	
+	
+	private DonorContactDetailsDTO  donorContactDetailsDTO;
+	private DonorAddressDetailsDTO donorAddressDetailsDTO;
+	
 	private Date lastDonationDate;
 	private Date nextAvailableDate;
+	
+	private ArrayList <DonationHistoryDTO> donationHistoryList = new ArrayList<DonationHistoryDTO>();
 	private Integer donationCount;
 	private Double distanceKm;
-	private boolean selectedDonor;
-
-	public Long getDonorId()
-	{
-		return donorId;
-	}
-
-	public void setDonorId(Long donorId)
-	{
-		this.donorId = donorId;
-	}
-
-	public String getDonorUuid()
-	{
-		return donorUuid;
-	}
-
-	public void setDonorUuid(String donorUuid)
-	{
-		this.donorUuid = donorUuid;
-	}
-
-	public String getFirstName()
-	{
+	
+	
+	public String getFirstName() {
 		return firstName;
 	}
-
-	public void setFirstName(String firstName)
-	{
+	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-
-	public String getLastName()
-	{
-		return lastName;
-	}
-
-	public void setLastName(String lastName)
-	{
-		this.lastName = lastName;
-	}
-
-	public String getMiddleName()
-	{
+	public String getMiddleName() {
 		return middleName;
 	}
-
-	public void setMiddleName(String middleName)
-	{
+	public void setMiddleName(String middleName) {
 		this.middleName = middleName;
 	}
-
-	public Date getBirthDate()
-	{
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Date getBirthDate() {
 		return birthDate;
 	}
-
-	public void setBirthDate(Date birthDate)
-	{
+	public void setBirthDate(Date birthDate) {
 		this.birthDate = birthDate;
 	}
-
-	public String getGender()
-	{
+	public String getGender() {
 		return gender;
 	}
-
-	public void setGender(String gender)
-	{
+	public void setGender(String gender) {
 		this.gender = gender;
 	}
-
-	public String getBloodGroup()
-	{
+	public String getBloodGroup() {
 		return bloodGroup;
 	}
-
-	public void setBloodGroup(String bloodGroup)
-	{
+	public void setBloodGroup(String bloodGroup) {
 		this.bloodGroup = bloodGroup;
 	}
-
-	public String getDonationComponentType()
-	{
+	public String getDonationComponentType() {
 		return donationComponentType;
 	}
-
-	public void setDonationComponentType(String donationComponentType)
-	{
+	public void setDonationComponentType(String donationComponentType) {
 		this.donationComponentType = donationComponentType;
 	}
-
-	public Integer getRating()
-	{
-		return rating;
-	}
-
-	public void setRating(Integer rating)
-	{
-		this.rating = rating;
-	}
-
-	public String getCreatedBy()
-	{
+	public String getCreatedBy() {
 		return createdBy;
 	}
-
-	public void setCreatedBy(String createdBy)
-	{
+	public void setCreatedBy(String createdBy) {
 		this.createdBy = createdBy;
 	}
-
-	public Date getCreatedDate()
-	{
+	public Date getCreatedDate() {
 		return createdDate;
 	}
-
-	public void setCreatedDate(Date createdDate)
-	{
+	public void setCreatedDate(Date createdDate) {
 		this.createdDate = createdDate;
 	}
-
-	public String getModifiedBy()
-	{
+	public String getModifiedBy() {
 		return modifiedBy;
 	}
-
-	public void setModifiedBy(String modifiedBy)
-	{
+	public void setModifiedBy(String modifiedBy) {
 		this.modifiedBy = modifiedBy;
 	}
-
-	public Date getModifiedDate()
-	{
+	public Date getModifiedDate() {
 		return modifiedDate;
 	}
-
-	public void setModifiedDate(Date modifiedDate)
-	{
+	public void setModifiedDate(Date modifiedDate) {
 		this.modifiedDate = modifiedDate;
 	}
-
-	public Date getLastDonationDate()
-	{
-		return lastDonationDate;
+	public Long getDonorId() {
+		return donorId;
 	}
-
-	public void setLastDonationDate(Date lastDonationDate)
-	{
-		this.lastDonationDate = lastDonationDate;
+	public void setDonorId(Long donorId) {
+		this.donorId = donorId;
 	}
-
-	public Date getNextAvailableDate()
-	{
-		return nextAvailableDate;
+	public String getDonorUuid() {
+		return donorUuid;
 	}
-
-	public void setNextAvailableDate(Date nextAvailableDate)
-	{
-		this.nextAvailableDate = nextAvailableDate;
+	public void setDonorUuid(String donorUuid) {
+		this.donorUuid = donorUuid;
 	}
-
-	public Integer getDonationCount()
-	{
-		return donationCount;
+	public DonorContactDetailsDTO getDonorContactDetailsDTO() {
+		return donorContactDetailsDTO;
 	}
-
-	public void setDonationCount(Integer donationCount)
-	{
-		this.donationCount = donationCount;
+	public void setDonorContactDetailsDTO(
+			DonorContactDetailsDTO donorContactDetailsDTO) {
+		this.donorContactDetailsDTO = donorContactDetailsDTO;
 	}
-
-	public Double getDistanceKm()
-	{
-		return distanceKm;
+	public DonorAddressDetailsDTO getDonorAddressDetailsDTO() {
+		return donorAddressDetailsDTO;
 	}
-
-	public void setDistanceKm(Double distanceKm)
-	{
-		this.distanceKm = distanceKm;
+	public void setDonorAddressDetailsDTO(
+			DonorAddressDetailsDTO donorAddressDetailsDTO) {
+		this.donorAddressDetailsDTO = donorAddressDetailsDTO;
 	}
-
-	public boolean isSelectedDonor()
-	{
+	public ArrayList<DonationHistoryDTO> getDonationHistoryList() {
+		return donationHistoryList;
+	}
+	public void setDonationHistoryList(
+			ArrayList<DonationHistoryDTO> donationHistoryList) {
+		this.donationHistoryList = donationHistoryList;
+	}
+	public boolean isSelectedDonor() {
 		return selectedDonor;
 	}
-
-	public void setSelectedDonor(boolean selectedDonor)
-	{
+	public void setSelectedDonor(boolean selectedDonor) {
 		this.selectedDonor = selectedDonor;
 	}
-
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public Date getLastDonationDate() {
+		return lastDonationDate;
+	}
+	public void setLastDonationDate(Date lastDonationDate) {
+		this.lastDonationDate = lastDonationDate;
+	}
+	public Date getNextAvailableDate() {
+		return nextAvailableDate;
+	}
+	public void setNextAvailableDate(Date nextAvailableDate) {
+		this.nextAvailableDate = nextAvailableDate;
+	}
+	public Integer getDonationCount() {
+		return donationCount;
+	}
+	public void setDonationCount(Integer donationCount) {
+		this.donationCount = donationCount;
+	}
+	public Double getDistanceKm() {
+		return distanceKm;
+	}
+	public void setDistanceKm(Double distanceKm) {
+		this.distanceKm = distanceKm;
+	}
+	
+	
 }
