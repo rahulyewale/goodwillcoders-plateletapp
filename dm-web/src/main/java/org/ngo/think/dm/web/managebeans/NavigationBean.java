6b051e33298214b4b2212ac2a15a60932a2fa71f
'rahuly' <rahul.yewale@mastek.com>
2015-04-07 14:25:10 +0530

    Added Secured access

---
+++ dm-web/src/main/java/org/ngo/think/dm/web/managebeans/NavigationBean.java
@@ -0,0 +1,65 @@
+package org.ngo.think.dm.web.managebeans;
+import java.io.Serializable;
+ 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+ 
+/**
+ * Simple navigation bean
+ *
+ */
+@ManagedBean
+@SessionScoped
+public class NavigationBean implements Serializable {
+ 
+    private static final long serialVersionUID = 1520318172495977648L;
+ 
+    /**
+     * Redirect to login page.
+     * @return Login page name.
+     */
+    public String redirectToLogin() {
+        return "/login.xhtml?faces-redirect=true";
+    }
+     
+    /**
+     * Go to login page.
+     * @return Login page name.
+     */
+    public String toLogin() {
+        return "/login.xhtml";
+    }
+     
+    /**
+     * Redirect to info page.
+     * @return Info page name.
+     */
+    public String redirectToInfo() {
+        return "/info.xhtml?faces-redirect=true";
+    }
+     
+    /**
+     * Go to info page.
+     * @return Info page name.
+     */
+    public String toInfo() {
+        return "/info.xhtml";
+    }
+     
+    /**
+     * Redirect to welcome page.
+     * @return Welcome page name.
+     */
+    public String redirectToWelcome() {
+        return "/secured/dashboard.xhtml?faces-redirect=true";
+    }
+     
+    /**
+     * Go to welcome page.
+     * @return Welcome page name.
+     */
+    public String toWelcome() {
+        return "/secured/dashboard.xhtml";
+    }
+     
+}
